a9a8f9fc567a38ba7039d3f87bdaf6bd3d40d2a9
Oliver Stenkilde <hello@stenkil.de>
2018-05-22 21:02:30 +0200

    Progress on timeline

--- src/pages/timeline/movies.js
+++ src/pages/timeline/movies.js
@@ -1,6 +1,118 @@
 export const movies = [
     {
         id: 1,
-        title: "Captain America: The First Avenger"
+        title: "Captain America: The First Avenger",
+        types: ["Origin"]
+    },
+    {
+        id: 2,
+        title: "Iron Man",
+        types: ["Origin"]
+    },
+    {
+        id: 3,
+        title: "Iron Man 2",
+        types: []
+    },
+    {
+        id: 4,
+        title: "The Incredible Hulk",
+        types: ["Origin"]
+    },
+    {
+        id: 5,
+        title: "Thor",
+        types: ["Origin"]
+    },
+    {
+        id: 6,
+        title: "Avengers",
+        types: ["Several Avengers"]
+    },
+    {
+        id: 7,
+        title: "Iron Man 3",
+        types: []
+    },
+    {
+        id: 8,
+        title: "Thor: The Dark World",
+        types: []
+    },
+    {
+        id: 9,
+        title: "Captain America: Winter Soldier",
+        types: []
+    },
+    {
+        id: 10,
+        title: "Guardians of the Galaxy",
+        types: ["Origin"]
+    },
+    {
+        id: 11,
+        title: "Guardians of the Galaxy Vol2",
+        types: []
+    },
+    {
+        id: 12,
+        title: "Avengers: Age of Ultron",
+        types: ["Several Avengers"]
+    },
+    {
+        id: 13,
+        title: "Ant-Man",
+        types: ["Origin"]
+    },
+    {
+        id: 14,
+        title: "Captain America: Civil War",
+        types: ["Several Avengers"]
+    },
+    {
+        id: 15,
+        title: "Doctor Strange",
+        types: ["Origin"]
+    },
+    {
+        id: 16,
+        title: "Spider-Man: Homecoming",
+        types: ["Origin"]
+    },
+    {
+        id: 17,
+        title: "Black Panther",
+        types: ["Origin"]
+    },
+    {
+        id: 18,
+        title: "Thor: Ragnarok",
+        types: []
+    },
+    {
+        id: 19,
+        title: "Avengers: Infinity War",
+        types: ["Several Avengers"]
     }
 ]
+
+
+// Captain America
+// Iron Man
+// Iron Man 2
+// Hulk (Can be skipped because it’s bad)
+// Thor
+// Avengers
+// Iron Man 3
+// Thor: The Dark World
+// Captain America: Winter Soldier
+// Guardians of the Galaxy
+// Guardians of the Galaxy 2
+// Avengers: Age of Ultron
+// Ant-Man
+// Captain America: Civil War
+// Doctor Strange
+// Spider-Man: Homecoming
+// Black Panther
+// Thor: Ragnarok
+// Infinity War
--- src/pages/timeline/timeline-item.js
+++ src/pages/timeline/timeline-item.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import './timeline.css';
 
 class TimelineItem extends Component {
     render() {
         console.log(this.props);
 
         return (
-            <div>
-                {this.props.movie.title}
+            <div className="tl-item">
+                <p className="title">{this.props.movie.title}</p>
+                {this.props.movie.types.map((type, index) => {
+                    return (
+                        <p className="type" key={index}>{type}</p>
+                    );
+                })}
             </div>
         );
     }
